6a0afa5b23f2cb10f7d5e3bfdc60d92637ae9b40
Artyom M <milch96@ya.ru>
2023-02-12 11:38:07 +0300

    reshil 68 zadachu

--- Program.cs
+++ Program.cs
@@ -26,31 +26,54 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30 (4+5+6+7+8=30)
 
-int ArifmP(int s, int f)
-{
-    int sum = 0;
-    sum = sum + s;
-    if(s == f-1) // пока конечное число не станет равным начальному
-    {
-        s++;
-        // Console.WriteLine(sum + " if " + " начальное " + s + " конечное " + f);
-        return sum += s;
-    }
-    else
-    {
-        // Console.WriteLine(sum + " else " + " начальное " + s + " конечное " + f);
-        return sum = s + ArifmP(s+1,f);
-    }
-}
+// int ArifmP(int s, int f)
+// {
+//     int sum = 0;
+//     sum = sum + s;
+//     if(s == f-1) // пока конечное число не станет равным начальному
+//     {
+//         s++;
+//         // Console.WriteLine(sum + " if " + " начальное " + s + " конечное " + f);
+//         return sum += s;
+//     }
+//     else
+//     {
+//         // Console.WriteLine(sum + " else " + " начальное " + s + " конечное " + f);
+//         return sum = s + ArifmP(s+1,f);
+//     }
+// }
 
-Console.WriteLine("Введите число, от которого надо складывать:");
-int start = Convert.ToInt32(Console.ReadLine()); // начальное число
-Console.WriteLine("Введите число, до которого надо складывать:");
-int final = Convert.ToInt32(Console.ReadLine()); // конечное число
-int sum = ArifmP(start, final);
-Console.WriteLine("Итоговая сумма: " + sum);
+// Console.WriteLine("Введите число, от которого надо складывать:");
+// int start = Convert.ToInt32(Console.ReadLine()); // начальное число
+// Console.WriteLine("Введите число, до которого надо складывать:");
+// int final = Convert.ToInt32(Console.ReadLine()); // конечное число
+// int sum = ArifmP(start, final);
+// Console.WriteLine("Итоговая сумма: " + sum);
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+int GetAck(int n, int m)
+{
+    if (n == 0) 
+    {
+        return m + 1;
+    }
+    if (n!=0 && m == 0) 
+    {
+        return GetAck(n - 1, 1);
+    }
+    else // n > 0 && m > 0
+    {
+        return (GetAck(n - 1, GetAck(n, m - 1)));
+    }
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine()); // начальное число
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine()); // конечное число
+int ackerman = GetAck(m, n);
+Console.WriteLine(ackerman);
